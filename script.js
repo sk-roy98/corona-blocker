87261c31c43f1f265044d9483eabf84e2fd83ed0
Souvik Roy <roysouvik98@gmail.com>
2021-05-31 20:58:09 +0530

    covidblock and wuhanvirus

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ function replaceText(element){
     if(element.hasChildNodes()){
         element.childNodes.forEach(replaceText)
     }else if(element.nodeType === Text.TEXT_NODE){
-        if(element.textContent.match(/coronavirus/gi)){
+        
+        element.textContent = element.textContent.replace(/coronavirus/gi,'Wuhanvirus')
+
+        if(element.textContent.match(/covid-19/gi)){
             const newElement = document.createElement('span')
 
-            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
+            newElement.innerHTML = element.textContent.replace(/(covid-19||covid 19|| covid)/gi,
                 '<span style = "background-color: #000; color: #000;">$1</span>')
             
             element.replaceWith(newElement)
-        }
-        //     element.parentElement.style.color = "black"
-        //     element.parentElement.style.backgroundColor = "black"
-        // }
-        // element.textContent = element.textContent.replace(/coronavirus/gi,'█████')
-
+        }   
+       
     };
 }
